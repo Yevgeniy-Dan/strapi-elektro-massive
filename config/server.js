3c99f4f0895f7210eee6016d2ce3a8776f15bd6b
Yevgeniy-Dan <geneforapp@gmail.com>
2024-08-06 15:26:27 +0300

    Resolved merge conflict in config/server.js

--- config/server.js
+++ config/server.js
@@ -1,9 +1,4 @@
 module.exports = ({ env }) => ({
-<<<<<<< HEAD
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'http://3.79.152.196:1337'),
-=======
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   url: env("PUBLIC_URL", "http://3.78.49.37:1337"),
@@ -11,7 +6,6 @@ module.exports = ({ env }) => ({
     url: "/admin",
     serveAdminPanel: true,
   },
->>>>>>> 48d142527fe5ad5b34f5b08bdb7e9f9abfeecf44
   app: {
     keys: env.array("APP_KEYS"),
   },
