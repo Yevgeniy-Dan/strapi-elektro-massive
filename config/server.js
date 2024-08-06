3ddebe76f36f34e0a0cf21bf950347638b0f0b33
Yevgeniy-Dan <geneforapp@gmail.com>
2024-08-06 14:47:15 +0300

    add public url to server

--- config/server.js
+++ config/server.js
@@ -1,6 +1,11 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'http://3.79.152.196:1337'),
+  admin: {
+    url: '/admin',
+    serveAdminPanel: true,
+  },
   app: {
     keys: env.array('APP_KEYS'),
   },
