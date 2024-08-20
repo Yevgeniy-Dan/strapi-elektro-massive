ec34fe361a50d2c35c48522fb529f88dba33d35a
Yevhenii Danoi <geneforapp@gmail.com>
2024-08-20 13:23:40 +0300

    change link filed in additional_images field of product from short to long text type

--- config/api.js
+++ config/api.js
@@ -1,6 +1,5 @@
 module.exports = {
   rest: {
-    prefix: '/admin',
     defaultLimit: 25,
     maxLimit: 100,
     withCount: true,
--- config/server.js
+++ config/server.js
@@ -2,10 +2,6 @@ module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   url: env("PUBLIC_URL", "https://admin.elektromassive.com"),
-  admin: {
-    url: "/admin",
-    serveAdminPanel: true,
-  },
   app: {
     keys: env.array("APP_KEYS"),
   },
